f1ca371f952cbf01aedc224cae978de0dfdc3662
Feeleep97 <38544335+Feeleep97@users.noreply.github.com>
2018-04-21 16:22:16 +0200

    Body beggining
    pocetak bodija

---
+++ body.php
@@ -0,0 +1,52 @@
+<?php include('html.php');
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+
+            <div class="container-fluid">
+                <div id="navigation">
+                    <ul class="top-level">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li>
+                            <a href="#">Portfolio</a>
+                            <ul class="sub-level">
+                                <li><a href="#">Logo Design</a></li>
+                                <li><a href="#">Mobile Apps</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                            <ul class="sub-level">
+                                <li><a href="#">Web Development</a>
+                                </li>
+                                <li><a href="#">Logo Design</a></li>
+                                <li>
+                                    <a href="#">Web Hosting</a>
+                                    <ul class="sub-level">
+                                        <li><a href="#">Linux</a></li>
+                                        <li><a href="#">Windows</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Feedback</a></li>
+                    </ul>
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
+
--- header.php
+++ header.php
@@ -26,42 +26,6 @@ include('html.php');
     </div>
 </nav>
 
-<div class="container-fluid">
-    <div id="navigation">
-        <ul class="top-level">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Contact</a></li>
-            <li>
-                <a href="#">Portfolio</a>
-                <ul class="sub-level">
-                    <li><a href="#">Logo Design</a></li>
-                    <li><a href="#">Mobile Apps</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Services</a>
-                <ul class="sub-level">
-                    <li><a href="#">Web Development</a>
-                    </li>
-                    <li><a href="#">Logo Design</a></li>
-                    <li>
-                        <a href="#">Web Hosting</a>
-                        <ul class="sub-level">
-                            <li><a href="#">Linux</a></li>
-                            <li><a href="#">Windows</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="#">Contact us</a></li>
-            <li><a href="#">Feedback</a></li>
-        </ul>
-    </div>
-
-
-
-</div>
 
 </body>
 </html>
