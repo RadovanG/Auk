1fcacc053373ae532e6ae9adaca2df97f92f4e50
Rikajlo <38581382+Rikajlo@users.noreply.github.com>
2018-04-21 13:42:39 +0200

    html i header za cofija
    u html se nalazi bootstrab link i meta tagovi

--- header.php
+++ header.php
@@ -1,7 +1,20 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: samsung
- * Date: 4/21/2018
- * Time: 1:04 PM
- */
+<?php include('html.php') ?>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
+</nav>
