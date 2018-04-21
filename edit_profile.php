f8b191022d64123070f7f82cdfdf0aa485ff155e
Radovan12345 <12116107@vts.su.ac.rs>
2018-04-21 18:48:19 +0200

    edit profile
    treba dodati unos slike

--- edit_profile.php
+++ edit_profile.php
@@ -1,16 +1,18 @@
+<?php if(!isset($_SESSION))
+{
+    session_start();
+}?>
 <?php
-session_start();
 include ('db_config.php');
-$id=$_SESSION['id_user'];
-$sql="SELECT * from users where id_user=$id";
+$id=$_SESSION['userID'];
+$sql="SELECT * from users where userID=$id";
 $query=mysqli_query($connection,$sql);
 
 if(mysqli_num_rows($query)>0)
 {
     while($row=mysqli_fetch_array($query))
     {
-        echo '<link href="css/bootstrap.css" rel="stylesheet">
-               <link href="css/custom_bootstrap.css" rel="stylesheet">
+        echo '
                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
                <link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet" type="text/css">
                <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">';
@@ -25,7 +27,6 @@ echo'<div class="container">
                     <label for="password" class="cols-sm-2 control-label">Password</label>
                     <div class="cols-sm-10">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
                             <input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
                         </div>
                     </div>
@@ -35,17 +36,35 @@ echo'<div class="container">
                     <label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
                     <div class="cols-sm-10">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
                             <input type="password" class="form-control" name="confirm" id="confirm"  placeholder="Confirm your Password"/>
                         </div>
                     </div>
                 </div>
 
+ <div class="form-group">
+                    <label for="firstName" class="cols-sm-2 control-label">First Name</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="firstName" id="firstName"  placeholder="Enter your First Name"/>
+                        </div>
+                    </div>
+                </div>
+                <span id="errorFirstName" hidden="hidden" style="color:red">Error First name must not be empty!</span>
+
+                <div class="form-group">
+                    <label for="lastName" class="cols-sm-2 control-label">Last Name</label>
+                    <div class="cols-sm-10">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="lastName" id="lastName"  placeholder="Enter your Last name"/>
+                        </div>
+                    </div>
+                </div>
+                <span id="errorLastName" hidden="hidden" style="color:red">Error Last name must not be empty!</span>
+                
                 <div class="form-group">
                     <label for="email" class="cols-sm-2 control-label">Your Email</label>
                     <div class="cols-sm-10">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
                             <input type="text" class="form-control" name="email" id="email" value="'.$row['email'].'" placeholder="Enter your Email"/>
                         </div>
                     </div>
@@ -56,7 +75,6 @@ echo'<div class="container">
                     <label for="address" class="cols-sm-2 control-label">Your address</label>
                     <div class="cols-sm-10">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
                             <input type="text" class="form-control" name="address" id="address"  value="'.$row['address'].'" placeholder="Enter your Address"/>
                         </div>
                     </div>
@@ -67,8 +85,7 @@ echo'<div class="container">
                     <label for="phone" class="cols-sm-2 control-label">Phone number</label>
                     <div class="cols-sm-10">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-                            <input type="text" class="form-control" name="phone" id="phone" value="'.$row['phone_number'].'"  placeholder="Enter your number"/>
+                            <input type="text" class="form-control" name="phone" id="phone" value="'.$row['phone'].'"  placeholder="Enter your number"/>
                         </div>
                     </div>
                 </div>
@@ -78,25 +95,12 @@ echo'<div class="container">
                     <label for="city" class="cols-sm-2 control-label">Your city</label>
                     <div class="cols-sm-10">
                         <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
                             <input type="text" class="form-control" name="city" id="city" value="'.$row['city'].'" placeholder="Enter your City"/>
                         </div>
                     </div>
                 </div>
                 <span id="errorCity" hidden="hidden" style="color:red">City must not be empty!</span>
-
-                <div class="form-group">
-                    <label for="country" class="cols-sm-2 control-label">Your Country</label>
-                    <div class="cols-sm-10">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-                            <input type="text" class="form-control" name="country" id="country" value="'.$row['country'].'" placeholder="Enter your Country"/>
-                        </div>
-                    </div>
-                </div>
-                <span id="errorCountry" hidden="hidden" style="color:red">Country must not be empty!</span>
-                <input type="text" id="id_user" hidden="hidden" value="'.$row['id_user'].'"
-                <div class="form-group ">
+                <input type="text" id="userID" hidden="hidden" value="'.$row['userID'].'">
                  
                     <input type="submit" id="submit" class="btn btn-primary btn-lg btn-block " style="background-color:#264d73;" value="Update">
                 </div>
