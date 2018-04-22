7f1bdb13d937f10dbb43b3d2543547d73643b3a0
Feeleep97 <38544335+Feeleep97@users.noreply.github.com>
2018-04-22 07:48:58 +0200

    zavrsen front end haha
    asdas

--- body.php
+++ body.php
@@ -64,14 +64,14 @@ VALUES ($articleID,$userIDins,$bid)";
                     <?php
                     while ($row = mysqli_fetch_array($result))
                     {
-                        echo "<li><a href='".$row['categoryID']."'>".$row['categoryName']."</a></li>";
+                        echo "<li><a style=\"color:#fff;\"href='".$row['categoryID']."'>".$row['categoryName']."</a></li>";
 
                         $sql1="SELECT * FROM subcategory WHERE categoryID = '".$row['categoryID']."'";
                         $result1 = $connection->query($sql1);
                         echo "<ul class='subcategory'>";
                         while ($row = mysqli_fetch_array($result1))
                         {
-                            echo "<li><a href='".$row['subcategoryID']."'>".$row['subcategoryName']."</a></li>";
+                            echo "<li><a style=\"color:#fff;\" href='".$row['subcategoryID']."'>".$row['subcategoryName']."</a></li>";
                         }
                         echo "</ul></li>";
                     }
